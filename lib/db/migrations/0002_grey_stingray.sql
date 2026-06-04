80f89c5cb4d56a3c9cae03e5711cd368e8317c53
Shane Linden <shanelinden1995@gmail.com>
2026-06-04 17:41:00 +0100

    fix: separate backfill into migration 0003 (0002 already applied on live db)

--- lib/db/migrations/0002_grey_stingray.sql
+++ lib/db/migrations/0002_grey_stingray.sql
@@ -1,9 +1,1 @@
 ALTER TABLE `invoice_line_items` ADD `unit_cost_incl_vat` real;
-
-UPDATE `invoice_line_items`
-SET `unit_cost_incl_vat` = CASE
-  WHEN `is_vatable` = 1
-    THEN `unit_cost` * (1 + (SELECT `vat_rate` FROM `invoices` WHERE `invoices`.`id` = `invoice_line_items`.`invoice_id`) / 100.0)
-  ELSE `unit_cost`
-END
-WHERE `unit_cost_incl_vat` IS NULL;
